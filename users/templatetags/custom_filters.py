6f7d7f4626d9a9b576d07d12a7148b985c0a22e2
lacry <lacryk@gmail.com>
2025-02-24 23:43:48 +0300

    moduls

--- users/templatetags/custom_filters.py
+++ users/templatetags/custom_filters.py
@@ -6,3 +6,15 @@ register = template.Library()
 @register.filter
 def add_class(field, css_class):
     return field.as_widget(attrs={"class": css_class})
+
+
+@register.filter(name='split')
+def split(value, delimiter='/'):
+    """Разбивает строку по разделителю и возвращает список."""
+    return value.split(delimiter)
+
+
+@register.filter
+def reject(value, arg):
+    """Удаляет указанный элемент из списка"""
+    return [item for item in value if item != arg]
