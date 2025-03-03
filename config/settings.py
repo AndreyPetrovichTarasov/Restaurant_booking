01da8add4db26e74e2c2d930f6c62f29c20382c4
lacry <lacryk@gmail.com>
2025-03-03 15:56:16 +0300

    moduls

--- config/settings.py
+++ config/settings.py
@@ -60,7 +60,7 @@ if "test" in sys.argv:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "test_db.sqlite3",
+            "NAME": str(BASE_DIR / "test_db.sqlite3"),  # Преобразуем в строку
         }
     }
 else:
